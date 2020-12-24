751e8c495926fc10145583d4d5c772a5ed0cccc5
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 11:59:04 -0600

    Set squares to be dynamic

--- js/script.js
+++ js/script.js
@@ -1,32 +1,13 @@
-function getClientWidth(){
-    if( typeof window.innerWidth === "number" ) {
-        return window.innerWidth;//Non-IE
-    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-        return document.documentElement.clientWidth;//IE 6+ in 'standards compliant mode'
-    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-        return document.body.clientWidth;//IE 4 compatible
-    }
-    return 0;
+let $canvas = $('#canvas'),
+    $rect = $('#rect'),
+    numRockets = 4,
+    rocketSeparation = window.innerWidth / numRockets;
+$canvas.attr("width", window.innerWidth.toString());
+$canvas.attr("height", window.innerHeight.toString());
+for (let i = 0; i < numRockets; i++) {
+    let $newRect = $rect.clone();
+    $newRect.attr("id", "rect" + i.toString());
+    $newRect.attr("x", (rocketSeparation * i).toString());
+    $newRect.attr("y", (window.innerHeight - $rect.attr("height")).toString());
+    $canvas.append($newRect);
 }
-/*
-    This function returns the height of the available screen real estate that we have
-*/
-function getClientHeight(){
-    if( typeof window.innerHeight === "number" ) {
-        return window.innerHeight;//Non-IE
-    } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-        return document.documentElement.clientHeight;//IE 6+ in 'standards compliant mode'
-    } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-        return document.body.clientHeight;//IE 4 compatible
-    }
-    return 0;
-}
-
-let $canvas = $('#canvas');
-$canvas.attr("width", getClientWidth().toString());
-$canvas.attr("height", getClientHeight().toString());
-$('#rect1').attr("y", (getClientHeight() - parseInt($('#rect1').attr("height"))).toString());
-$('#rect2').attr("x", (getClientWidth() / 2 - parseInt($('#rect2').attr("width")) / 2).toString());
-$('#rect2').attr("y", (getClientHeight() - parseInt($('#rect2').attr("height"))).toString());
-$('#rect3').attr("x", (getClientWidth() - parseInt($('#rect3').attr("width"))).toString());
-$('#rect3').attr("y", (getClientHeight() - parseInt($('#rect3').attr("height"))).toString());
