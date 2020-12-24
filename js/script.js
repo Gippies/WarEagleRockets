9b28f3f046512866737c476acb56c55a1dcd1404
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 12:13:11 -0600

    Made screenWidth and screenHeight variables

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 let $canvas = $('#canvas'),
     $rect = $('#rect'),
     numRockets = 4,
-    rocketSeparation = window.innerWidth / numRockets;
-$canvas.attr("width", window.innerWidth.toString());
-$canvas.attr("height", window.innerHeight.toString());
+    screenWidth = $(window).width(),
+    screenHeight = $(window).height(),
+    rocketSeparation = screenWidth / (numRockets - 1);
+$canvas.attr("width", screenWidth.toString());
+$canvas.attr("height", screenHeight.toString());
 for (let i = 0; i < numRockets; i++) {
     let $newRect = $rect.clone();
     $newRect.attr("id", "rect" + i.toString());
     $newRect.attr("x", (rocketSeparation * i).toString());
-    $newRect.attr("y", (window.innerHeight - $rect.attr("height")).toString());
+    $newRect.attr("y", (screenHeight - $rect.attr("height")).toString());
     $canvas.append($newRect);
 }
