df80fe7d9a06481822e159d5f4b7c2e140744127
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 13:04:42 -0600

    Implemented better widths for the rockets relative to the screen

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ let $canvas = $('#canvas'),
     numRockets = 4;
 
 $(window).on("load resize", function () {
-    let rocketSeparation = $(window).width() / (numRockets - 1);
+    let rocketSeparation = ($(window).width() * 10 / 12) / (numRockets - 1);
     $canvas.attr("width", $(window).width().toString());
     $canvas.attr("height", $(window).height().toString());
     $canvas.empty();
     for (let i = 0; i < numRockets; i++) {
         let $newRect = $rect.clone();
         $newRect.attr("id", "rect" + i.toString());
-        $newRect.attr("x", (rocketSeparation * i - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
+        $newRect.attr("x", (($(window).width() * (1 / 12) + rocketSeparation * i) - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
         $newRect.attr("y", ($canvas.attr("height") - $rect.attr("height")).toString());
         $canvas.append($newRect);
     }
