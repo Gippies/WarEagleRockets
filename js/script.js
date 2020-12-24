6d165382ea4e33ceebe8facda6ea8fa01bda6b30
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 15:19:01 -0600

    Added rising of the rockets (need to fix resizing causing a reset)

--- js/script.js
+++ js/script.js
@@ -21,9 +21,16 @@ $(window).on("load resize", function () {
         let newBtnX = parseInt($newRect.attr("x")) + parseInt($newRect.attr("width")) / 2,
             newBtnY = parseInt($newRect.attr("y")) + parseInt($newRect.attr("height")) + btnHeightOffset;
         $newPlusOneBtn.attr("id", "plusOneBtn" + i.toString());
+        $newPlusOneBtn.attr("data-rocket", "#rect" + i.toString());
         $newPlusOneBtn.css({"left": newBtnX.toString() + "px", "top": newBtnY.toString() + "px"});
 
         $canvas.append($newRect);
         $buttonsDiv.append($newPlusOneBtn);
     }
+
+    $('.btn').on("click", function (event) {
+        event.preventDefault();
+        let $currentRocket = $($(this).data("rocket"));
+        $currentRocket.attr("y", (parseInt($currentRocket.attr("y")) - 50).toString());
+    });
 });
