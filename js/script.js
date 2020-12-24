bf972f79955eba522d8d08ab0513c00c140a71a7
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 12:30:41 -0600

    Fixed screen overflow issues

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 let $canvas = $('#canvas'),
     $rect = $('#rect'),
     numRockets = 4,
-    screenWidth = $(window).width(),
-    screenHeight = $(window).height(),
-    rocketSeparation = screenWidth / (numRockets - 1);
-$canvas.attr("width", screenWidth.toString());
-$canvas.attr("height", screenHeight.toString());
+    rocketSeparation = $(window).width() / (numRockets - 1);
+$canvas.attr("width", $(window).width().toString());
+$canvas.attr("height", $(window).height().toString());
+$canvas.empty();
 for (let i = 0; i < numRockets; i++) {
     let $newRect = $rect.clone();
     $newRect.attr("id", "rect" + i.toString());
-    $newRect.attr("x", (rocketSeparation * i).toString());
-    $newRect.attr("y", (screenHeight - $rect.attr("height")).toString());
+    $newRect.attr("x", (rocketSeparation * i - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
+    $newRect.attr("y", ($canvas.attr("height") - $rect.attr("height")).toString());
     $canvas.append($newRect);
 }
