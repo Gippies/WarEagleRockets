12a7812d3e25d12fdf8c39b3e2a828098ed982c0
Joshua Patterson <jleegippies@gmail.com>
2020-12-24 12:38:16 -0600

    Got the elements to change position based on the screen's size

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let $canvas = $('#canvas'),
     $rect = $('#rect'),
-    numRockets = 4,
-    rocketSeparation = $(window).width() / (numRockets - 1);
-$canvas.attr("width", $(window).width().toString());
-$canvas.attr("height", $(window).height().toString());
-$canvas.empty();
-for (let i = 0; i < numRockets; i++) {
-    let $newRect = $rect.clone();
-    $newRect.attr("id", "rect" + i.toString());
-    $newRect.attr("x", (rocketSeparation * i - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
-    $newRect.attr("y", ($canvas.attr("height") - $rect.attr("height")).toString());
-    $canvas.append($newRect);
-}
+    numRockets = 4;
+
+$(window).on("load resize", function () {
+    let rocketSeparation = $(window).width() / (numRockets - 1);
+    $canvas.attr("width", $(window).width().toString());
+    $canvas.attr("height", $(window).height().toString());
+    $canvas.empty();
+    for (let i = 0; i < numRockets; i++) {
+        let $newRect = $rect.clone();
+        $newRect.attr("id", "rect" + i.toString());
+        $newRect.attr("x", (rocketSeparation * i - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
+        $newRect.attr("y", ($canvas.attr("height") - $rect.attr("height")).toString());
+        $canvas.append($newRect);
+    }
+});
