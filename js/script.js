12899cb9839d6c148e52e686ad970a5655c8c4d1
Joshua Patterson <jleegippies@gmail.com>
2020-12-26 20:05:09 -0600

    Got vertical scaling to work properly

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ let $canvas = $('#canvas'),
     $rect = $('#rect'),
     $plusOneBtn = $('#plusOneBtn'),
     rocketYPositions = {},
-    btnYPosition = -1,
     numRockets = 4,
-    btnHeightOffset = 50,
-    rocketSpeed = 50;
+    btnYStartPosition = 18,
+    rocketYStartPosition = 16,
+    canvasHeightDifference = 1 / 20;
 
 $(window).on("load resize", function () {
     let rocketSeparation = ($(window).width() * 10 / 12) / (numRockets - 1);
@@ -20,25 +20,19 @@ $(window).on("load resize", function () {
         $newRect.attr("id", "rect" + i.toString());
         $newRect.attr("x", (($(window).width() * (1 / 12) + rocketSeparation * i) - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
         if (rocketYPositions[i] === undefined) {
-            $newRect.attr("y", (($canvas.attr("height") - $canvas.attr("height") * (1 / 5)) - $newRect.attr("height")).toString());
-            rocketYPositions[i] = parseInt($newRect.attr("y"));
+            $newRect.attr("y", (parseInt($canvas.attr("height")) * rocketYStartPosition * canvasHeightDifference).toString());
+            rocketYPositions[i] = rocketYStartPosition;
         }
         else {
-            $newRect.attr("y", rocketYPositions[i].toString());
+            $newRect.attr("y", (parseInt($canvas.attr("height")) * rocketYPositions[i] * canvasHeightDifference).toString());
         }
 
         $newPlusOneBtn.attr("id", "plusOneBtn" + i.toString());
         $newPlusOneBtn.attr("data-rocket-id", "#rect" + i.toString());
         $newPlusOneBtn.attr("data-rocket-number", i.toString());
-        let newBtnX = parseInt($newRect.attr("x")) + parseInt($newRect.attr("width")) / 2;
-        if (btnYPosition === -1) {
-            let newBtnY = parseInt($newRect.attr("y")) + parseInt($newRect.attr("height")) + btnHeightOffset;
-            $newPlusOneBtn.css({"left": newBtnX.toString() + "px", "top": newBtnY.toString() + "px"});
-            btnYPosition = newBtnY;
-        }
-        else {
-            $newPlusOneBtn.css({"left": newBtnX.toString() + "px", "top": btnYPosition.toString() + "px"});
-        }
+        let newBtnX = parseInt($newRect.attr("x")) + parseInt($newRect.attr("width")) / 2,
+            newBtnY = parseInt($canvas.attr("height")) * btnYStartPosition * canvasHeightDifference;
+        $newPlusOneBtn.css({"left": newBtnX.toString() + "px", "top": newBtnY.toString() + "px"});
 
         $canvas.append($newRect);
         $buttonsDiv.append($newPlusOneBtn);
@@ -46,8 +40,9 @@ $(window).on("load resize", function () {
 
     $('.btn').on("click", function (e) {
         e.preventDefault();
-        let $currentRocket = $($(this).data("rocket-id"));
-        $currentRocket.attr("y", (parseInt($currentRocket.attr("y")) - rocketSpeed).toString());
-        rocketYPositions[parseInt($(this).data("rocket-number"))] = parseInt($currentRocket.attr("y"));
+        let $currentRocket = $($(this).data("rocket-id")),
+            i = parseInt($(this).data("rocket-number"));
+        rocketYPositions[i]--;
+        $currentRocket.attr("y", (parseInt($canvas.attr("height")) * rocketYPositions[i] * canvasHeightDifference).toString());
     });
 });
