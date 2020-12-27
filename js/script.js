0071cfd941ada9eb56df3c7e45d8ea38178d3b5c
Joshua Patterson <jleegippies@gmail.com>
2020-12-27 11:16:11 -0600

    Added rockets!!

--- js/script.js
+++ js/script.js
@@ -1,40 +1,39 @@
-let $canvas = $('#canvas'),
+let $rocketsDiv = $('#rocketsDiv'),
     $buttonsDiv = $('#buttonsDiv'),
-    $rect = $('#rect'),
+    $initialRocket = $('#initialRocket'),
     $plusOneBtn = $('#plusOneBtn'),
     rocketYPositions = {},
     numRockets = 4,
+    rocketWidth = 200,  // Be sure to synchronize this in the css file!!
     btnYStartPosition = 18,
-    rocketYStartPosition = 16,
-    canvasHeightDifference = 1 / 20;
+    rocketYStartPosition = 11,
+    windowHeightDifference = 1 / 20;
 
 $(window).on("load resize", function () {
     let rocketSeparation = ($(window).width() * 10 / 12) / (numRockets - 1);
-    $canvas.attr("width", $(window).width().toString());
-    $canvas.attr("height", $(window).height().toString());
-    $canvas.empty();
+    $rocketsDiv.empty();
     $buttonsDiv.empty();
     for (let i = 0; i < numRockets; i++) {
-        let $newRect = $rect.clone(),
+        let $newRocket = $initialRocket.clone(),
             $newPlusOneBtn = $plusOneBtn.clone();
-        $newRect.attr("id", "rect" + i.toString());
-        $newRect.attr("x", (($(window).width() * (1 / 12) + rocketSeparation * i) - parseInt($newRect.attr("width")) * i / (numRockets - 1)).toString());
+        $newRocket.attr("id", "rocket" + i.toString());
+        $newRocket.css("left", (($(window).width() * (1 / 12) + rocketSeparation * i) - rocketWidth * i / (numRockets - 1)).toString() + "px");
         if (rocketYPositions[i] === undefined) {
-            $newRect.attr("y", (parseInt($canvas.attr("height")) * rocketYStartPosition * canvasHeightDifference).toString());
+            $newRocket.css("top", ($(window).height() * rocketYStartPosition * windowHeightDifference).toString() + "px");
             rocketYPositions[i] = rocketYStartPosition;
         }
         else {
-            $newRect.attr("y", (parseInt($canvas.attr("height")) * rocketYPositions[i] * canvasHeightDifference).toString());
+            $newRocket.css("top", ($(window).height() * rocketYPositions[i] * windowHeightDifference).toString() + "px");
         }
 
         $newPlusOneBtn.attr("id", "plusOneBtn" + i.toString());
-        $newPlusOneBtn.attr("data-rocket-id", "#rect" + i.toString());
+        $newPlusOneBtn.attr("data-rocket-id", "#rocket" + i.toString());
         $newPlusOneBtn.attr("data-rocket-number", i.toString());
-        let newBtnX = parseInt($newRect.attr("x")) + parseInt($newRect.attr("width")) / 2,
-            newBtnY = parseInt($canvas.attr("height")) * btnYStartPosition * canvasHeightDifference;
+        let newBtnX = parseInt($newRocket.css("left")) + rocketWidth / 2,
+            newBtnY = $(window).height() * btnYStartPosition * windowHeightDifference;
         $newPlusOneBtn.css({"left": newBtnX.toString() + "px", "top": newBtnY.toString() + "px"});
 
-        $canvas.append($newRect);
+        $rocketsDiv.append($newRocket);
         $buttonsDiv.append($newPlusOneBtn);
     }
 
@@ -43,6 +42,6 @@ $(window).on("load resize", function () {
         let $currentRocket = $($(this).data("rocket-id")),
             i = parseInt($(this).data("rocket-number"));
         rocketYPositions[i]--;
-        $currentRocket.attr("y", (parseInt($canvas.attr("height")) * rocketYPositions[i] * canvasHeightDifference).toString());
+        $currentRocket.css("top", ($(window).height() * rocketYPositions[i] * windowHeightDifference).toString() + "px");
     });
 });
